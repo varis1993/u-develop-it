4a8f4f51db88c6ae1d66bd6095a1235e4812a438
Tavaris Thompson <varis93@yahoo.com>
2022-01-11 19:29:14 -0500

    errors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
+    password: 'Monkey123@',
     database: 'election'
   },
   console.log('Connected to the election database.')
